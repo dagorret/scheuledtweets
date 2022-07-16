417e09373c012256646fad3d67d2653a5e750fcb
Carlos Dagorret <carlos@dagorret.com.ar>
2022-07-16 17:45:02 -0300

    remove flash test in index application

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,6 @@
 class MainController < ApplicationController
     
     def index
-        flash.now[:notice] = "Logged in successfully"
-        flash.now[:alert] = "Invalid email o password"
     end
     
 end
